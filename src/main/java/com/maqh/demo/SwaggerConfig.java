711300e624dc0124f4828043e70cdee3596e91ae
maqh <maqinghua02@gmail.com>
2019-11-21 17:45:34 +0800

    init

--- src/main/java/com/maqh/demo/SwaggerConfig.java
+++ src/main/java/com/maqh/demo/SwaggerConfig.java
@@ -1,25 +1,15 @@
 package com.maqh.demo;
 
 
-import io.swagger.annotations.ApiOperation;
-import io.swagger.models.Contact;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @author maqh
